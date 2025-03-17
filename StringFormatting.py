c9cd88dbbc87e536f7b61e068e6538c66b1929f1
royrga <royrga@gmail.com>
2025-03-17 14:57:11 -0600

    String formatting comment printf

--- StringFormatting.py
+++ StringFormatting.py
@@ -10,7 +10,7 @@ def print_formatted(number):
     for i in range(1,number+1):
         print(f"{str(i).rjust(max_l)} {n_oct[i-1].rjust(max_l)} {n_hex[i-1].rjust(max_l)} {n_bin[i-1].rjust(max_l)}")
     
-    print(max_l)
+    #print(max_l)
     #print(f"{n} {oct(n).lstrip("0o"):>} {hex(n).lstrip("0x"):>} {bin(n).lstrip("0b"):>}")
 
 
